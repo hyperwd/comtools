4929370e9c7e740ff1fdcb2a62f6a63ee8bd36da
hyperwd <435904632@qq.com>
2017-12-20 08:28:39 +0800

    da

--- spider/scripts/threading_one.py
+++ spider/scripts/threading_one.py
@@ -4,29 +4,44 @@ import threading
 import time
 import os
 
-I = 1000
-LOCK = threading.Lock()
+#I = 100
+#LOCK = threading.Lock()
+#
+#def do_other():
+#    '''this func could be any func to do other works.'''
+#    time.sleep(0.5)
+#
+#def booth(threads):
+#    '''func for each thread'''
+#    global I
+#    global LOCK
+#    while True:
+##        LOCK.acquire()
+#        if I != 0:
+#            I -= 1
+#            time.sleep(1)
+#            print(threads, ':now left:', I)
+#            do_other()
+#        else:
+#            print(threads, 'no more tickets')
+#            os._exit(0)
+##        LOCK.release()
+#        do_other()
+#
+#for k in range(10):
+#    newThread = threading.Thread(target=booth, args=(k, ))
+#    newThread.start()
 
-def do_other():
-    '''this func could be any func to do other works.'''
-    time.sleep(0.5)
+class MyST(threading.Thread):
+    '''ddddd'''
+    def __init__(self, tid, ilist):
+        '''ddd'''
+        threading.Thread.__init__(self)
+        self.tid = tid
+        self.ilist = ilist
 
-def booth(threads):
-    '''func for each thread'''
-    global I
-    global LOCK
-    while True:
-#        LOCK.acquire()
-        if I != 0:
-            I -= 1
-            print(threads, ':now left:', I)
-            do_other()
-        else:
-            print(threads, 'no more tickets')
-            os._exit(0)
-#        LOCK.release()
-        do_other()
-
-for k in range(10):
-    newThread = threading.Thread(target=booth, args=(k, ))
-    newThread.start()
+    def run():
+        '''ddd'''
+        while True:
+            ilist['lock'].acquire()
+            if ilist['ticket']
