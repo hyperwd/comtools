09abcd2c99b3cc726a727c85f68a3eb4baede004
hyperwd <dongwei_chao@163.com>
2018-04-29 23:39:05 +0800

    ddok

--- rpms/vim/ins_after.sh
+++ rpms/vim/ins_after.sh
@@ -2,4 +2,6 @@
 
 source /etc/rc.d/init.d/functions
 ln -s /usr/local/vim/bin/vim /usr/bin/vim
+ln -s /usr/local/vim/bin/vimtutor /usr/bin/vimtutor
+ln -s /usr/local/vim/bin/vimdiff /usr/bin/vimdiff
 exit $?
--- rpms/vim/rem_after.sh
+++ rpms/vim/rem_after.sh
@@ -3,4 +3,6 @@
 source /etc/rc.d/init.d/functions
 rm -rf /usr/local/vim
 rm -rf /usr/bin/vim
+rm -rf /usr/bin/vimtutor
+rm -rf /usr/bin/vimdiff
 exit $?
